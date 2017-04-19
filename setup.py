2ee9fee2eeda09ced88515a34bb5db0b446f1915
Lee Scholefield <leescholefield@gmail.com>
2017-04-19 15:09:14 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Parser',
+      version='1.0',
+      description='Library for parsing a document.',
+      author='Lee Scholefield',
+      url='https://github.com/leescholefield/parser.git',
+      )
