3b3777d1f539b1c1b406931824734e08ecb847dc
Lee Scholefield <leescholefield@gmail.com>
2017-04-19 16:10:38 +0100

    Change pachage_dir

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='Parser',
       author='Lee Scholefield',
       url='https://github.com/leescholefield/parser.git',
       packages=['parser'],
-      package_dir=['parser/parser']
+      package_dir={'parser': 'parser/parser'}
       )
