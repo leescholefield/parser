9cc38d0ee3c492d5cbbb581bd5a4c60d92f1cba8
Lee Scholefield <leescholefield@gmail.com>
2017-04-25 21:35:03 +0100

    Added tests for the Parser._get_attribute_dict and Parser._convert_to_type methods.

--- test/parse_test.py
+++ test/parse_test.py
@@ -3,18 +3,20 @@ import unittest
 
 from apl.attributes import Attribute, AttributeList
 from apl.parse import Parser, XmlParser
-from .data.models import RevolutionsEpisode
+from test.data.models import RevolutionsEpisode
 
 
-class ParserTest(unittest.TestCase):
+class ParserClassTest(unittest.TestCase):
+    """ Tests the Parser instantiation methods and the class Static methods. """
 
     def test_from_string_returns_XmlParser(self):
+        """ Tests whether the from_string method with nothing but a string returns an XmlParser instance. """
         file = io.open('data/revolutions_feed.xml').read()
         actual = Parser.from_string(file)
 
         self.assertEqual(XmlParser, type(actual))
 
-    def test_get_attr_list_obj(self):
+    def test_get_attr_obj(self):
         """ Tests whether get_attribute_list finds the Attributes from an object and returns a dict."""
         a1 = Attribute(['location'])
         a2 = Attribute(['second location'])
@@ -27,7 +29,7 @@ class ParserTest(unittest.TestCase):
 
         self.assertDictEqual(expected, {'attr1': a1, 'attr2': a2})
 
-    def test_get_attr_list_dict(self):
+    def test_get_attr_dict(self):
         """ Tests whether get_attribute_list finds the Attributes from a dict. """
         a1 = Attribute(['location'])
         a2 = Attribute(['second location'])
@@ -36,6 +38,32 @@ class ParserTest(unittest.TestCase):
         expected = {'attr1': a1, 'attr2': a2}
 
         self.assertDictEqual(expected, Parser._get_attribute_dict(dict_))
+
+    def test_get_attr_dict_invalid_obj(self):
+        """ Tests whether passing an object with no Attributes will raise a ValueError. """
+
+        class Example:
+            pass
+
+        self.assertRaises(ValueError, Parser._get_attribute_dict, Example)
+
+    def test_get_attr_dict_invalid_dic(self):
+        """ Tests whether passing a dictionary with no Attribute will raise a ValueError."""
+        dict_ = {}
+
+        self.assertRaises(ValueError, Parser._get_attribute_dict, dict_)
+
+    def test_convert_to_type(self):
+        """ Tests the Parser._convert_to_type method with a string to int. """
+        string_num = '210'
+
+        self.assertEqual(Parser._convert_to_type(string_num, int), 210)
+
+    def test_convert_to_type_invalid(self):
+        """ Tests that an invalid String to int conversion will throw an ValueError. """
+        invalid_string = 'asdasda'
+
+        self.assertRaises(ValueError, Parser._convert_to_type, invalid_string, int)
 
 
 class RevolutionsTest(unittest.TestCase):
@@ -46,7 +74,7 @@ class RevolutionsTest(unittest.TestCase):
         self.parser = Parser.from_string(file)
 
     def test_parse_attribute(self):
-        attr = Attribute('channel/title/text()')
+        attr = Attribute(['channel/title/text()'])
         root = self.parser.root
 
         self.assertEqual(self.parser.parse_attribute(attr, root=root), 'Revolutions')
@@ -64,7 +92,7 @@ class RevolutionsTest(unittest.TestCase):
         def parse_method(string):
             return 'parse method string'
 
-        attr = Attribute('channel/title/text()', parse_method=parse_method)
+        attr = Attribute(['channel/title/text()'], parse_method=parse_method)
 
         expected = self.parser.parse_attribute(attr)
 
@@ -74,8 +102,8 @@ class RevolutionsTest(unittest.TestCase):
 class HtmlTest(unittest.TestCase):
 
     class HtmlModel:
-        title = Attribute('body/h1/text()')
-        second_para = Attribute('body/div/p/text()')
+        title = Attribute(['body/h1/text()'])
+        second_para = Attribute(['body/div/p/text()'])
 
     def setUp(self):
         import io
